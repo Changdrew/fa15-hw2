7574374f0dc12d2bfe8e36d515a0757b51567713
Andrew Chang <changdrew@berkeley.edu>
2015-09-25 18:49:24 -0700

    finished

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,22 @@
 class PagesController < ApplicationController
   def home
-    # foo = Foobar.new "baz"
-    # @baz = foo.bar :cat, sat: :dat, dat: :sat
+    foo = Foobar.new "baz"
+    @baz = foo.bar :cat, sat: :dat, dat: :sat
   end
 
   def stringify
     @text = "You are nothing!"
+    @name = params[:name]
+    @adjective = params[:adjective]
   end
 
   def age
   end
 
   def person
+    input = Person.new params[:name], params[:age]
+    @inputIntro = input.introduce
+    @inputBirthYear = input.birth_year
+    @inputNickname = input.nickname
   end
 end
